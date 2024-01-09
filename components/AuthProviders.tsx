25918513aad33c3272e3206297461494be527e44
Shiyamal Kumar <shiyamal.asokan@bounteous.com>
2024-01-09 13:32:25 +0530

    verify google html

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+"use client";
+
+import { getProviders } from "next-auth/react";
+import { useState, useEffect } from "react";
+
+type Provider = {
+  id: string;
+  name: string;
+  type: string;
+  signinurl: string;
+  callbackURl: string;
+  signUrlParams: Record<string, string> | null;
+};
+type Providers = Record<string, Provider>;
 
 const AuthProviders = () => {
-  return (
-    <div>AuthProviders</div>
-  )
-}
+  const [providers, setProviders] = useState<any | null>(null);
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const res = await getProviders();
+      setProviders(res);
+      console.log(res);
+    };
+    fetchProviders();
+  }, []);
+  if (providers) {
+    return (
+      <div>
+        {Object.values(providers).map((provider: any, i) => (
+          <button key={i}>{provider.id}</button>
+        ))}
+      </div>
+    );
+  }
+};
 
-export default AuthProviders
+export default AuthProviders;
